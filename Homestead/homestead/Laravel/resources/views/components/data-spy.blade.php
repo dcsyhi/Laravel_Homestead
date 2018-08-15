1c94a806cc7204270c2e2fe90ed27e7815a3fe7e
dcsyhi <takanamoo@gmail.com>
2018-08-15 22:22:46 +0900

    add scroll-spy

---
+++ Homestead/homestead/Laravel/resources/views/components/data-spy.blade.php
@@ -0,0 +1,38 @@
+<!-- Custom css that makes this example work like it does: -->
+<style type="text/css" scoped="">
+.scrollspy-example {
+    position: relative;
+    height: 200px;
+    margin-top: .5rem;
+    overflow: auto;
+}
+</style>
+
+<!-- Actual scrollspy markup: -->
+<nav id="navbar-example2" class="navbar navbar-light bg-light">
+  <ul class="nav nav-pills">
+    <li class="nav-item"><a class="nav-link active" href="#village1">1日目</a></li>
+    <li class="nav-item"><a class="nav-link" href="#village2">2日目</a></li>
+    <li class="nav-item"><a class="nav-link" href="#village3">3日目</a></li>
+    <li class="nav-item"><a class="nav-link" href="#village4">4日目</a></li>
+    <li class="nav-item"><a class="nav-link" href="#village5">5日目</a></li>
+    <li class="nav-item"><a class="nav-link" href="#village6">6日目</a></li>
+    <li class="nav-item"><a class="nav-link" href="#village7">7日目</a></li>
+  </ul>
+</nav>
+<div data-spy="scroll" data-target="#navbar-example2" data-offset="0" class="scrollspy-example">
+  <h4 id="village1">1日目</h4>
+  <p>May <br> the <br> gods <br> forgive <br> me.</p>
+  <h4 id="village2">2日目</h4>
+  <p>For <br> this <br> rampant <br> abuse <br> of <br> br-tags.</p>
+  <h4 id="village3">3日目</h4>
+  <p>For <br> this <br> rampant <br> abuse <br> of <br> br-tags.</p>
+  <h4 id="village4">4日目</h4>
+  <p>For <br> this <br> rampant <br> abuse <br> of <br> br-tags.</p>
+  <h4 id="village5">5日目</h4>
+  <p>For <br> this <br> rampant <br> abuse <br> of <br> br-tags.</p>
+  <h4 id="village6">6日目</h4>
+  <p>For <br> this <br> rampant <br> abuse <br> of <br> br-tags.</p>
+  <h4 id="village7">7日目</h4>
+  <p>For <br> this <br> rampant <br> abuse <br> of <br> br-tags.</p>
+</div>
