bc3f55b062f4a46e9a7463efc347c1e53fd128cd
dcsyhi <takanamoo@gmail.com>
2018-08-15 11:56:50 +0900

    ページごとのcss/js作成

--- Homestead/homestead/Laravel/resources/views/page/index.blade.php
+++ Homestead/homestead/Laravel/resources/views/page/index.blade.php
@@ -3,9 +3,11 @@
 @section('title', 'ページタイトル')
 @section('keywords', 'A,B,C')
 @section('description', '説明文')
- 
+@section('pageCss')
+<link href="{{ asset('css/page.css') }}" rel="stylesheet">
+@endsection
 @include('layouts.head')
- 
+
 @include('layouts.header')
  
 @section('content')
@@ -16,6 +18,10 @@
  
 @section('pageSub')
     <p>個別サイドバーの内容</p>
+@endsection
+ 
+@section('pageJs')
+<script src="{{ asset('js/page.js') }}"></script>
 @endsection
  
 @include('layouts.footer')
